4ce0657ff6752f9caa2e929e4e0ea4426f52c708
Dinar Davis <dinardavis@gmail.com>
2025-09-02 19:00:10 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("vite");
 const react = require("@vitejs/plugin-react");
 
-// https://vitejs.dev/config/
 module.exports = defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
+  base: "/rochelle_ruen/", 
 });
